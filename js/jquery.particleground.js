e1f5a2512943cedc865e2f4c5387dcf5ee5afaf1
Manjunath N <56718219+Manjuchandan@users.noreply.github.com>
2020-06-14 22:28:49 +0530

    Update jquery.particleground.js

--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -373,16 +373,16 @@
   };
 
   window[pluginName].defaults = {
-    minSpeedX: 0.1,
-    maxSpeedX: 0.7,
-    minSpeedY: 0.1,
-    maxSpeedY: 0.7,
-    directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
-    directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+    minSpeedX: 0.2,
+    maxSpeedX: 0.6,
+    minSpeedY: 0.2,
+    maxSpeedY: 0.6,
+    directionX: 'left', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+    directionY: 'right', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
-    dotColor: '#666666',
-    lineColor: '#666666',
-    particleRadius: 7, // Dot size
+    dotColor: '#1B4F72',
+    lineColor: '#1B4F72',
+    particleRadius: 9, // Dot size
     lineWidth: 1,
     curvedLines: false,
     proximity: 100, // How close two dots need to be before they join
